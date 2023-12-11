9a3546a0478985b1b6c305dbbde718b167120a82
headboyprince <headboyprince@gmail.com>
2023-12-11 09:23:26 +0100

    Update config.js

--- config.js
+++ config.js
@@ -7,13 +7,13 @@ import axios from 'axios'
 
 
 global.owner = [
-  ['2348056362226', 'kelvin WhatsApp bot, true],
+  ['2349153801054', 'kelvin WhatsApp bot, true],
   [''],
   [''] 
 ] //Number of owners
 
-global.mods = ['2348056362226','2348056362226'] 
-global.prems = ['2348056362226', '2348056362226', '2348056362226']
+global.mods = ['2349153801054','2349153801054'] 
+global.prems = ['2349153801054', '2349153801054', '2349153801054']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
 global.keysxteammm = ['29d4b59a4aa687ca', '5LTV57azwaid7dXfz5fzJu', 'cb15ed422c71a2fb', '5bd33b276d41d6b4', 'HIRO', 'kurrxd09', 'ebb6251cc00f9c63']
